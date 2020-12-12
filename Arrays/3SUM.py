432f36f55bc6173237918afbbf39782e79825da5
Varsha Budihal <34867211+varshabudihal@users.noreply.github.com>
2020-12-12 13:34:15 -0800

    Update 3SUM.py
    Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero. 
    Notice that the solution set must not contain duplicate triplets.
    
    Solution:
    
    In order to solve this question, I first reduced it to 2SUM problem by doing a+b = -c and the duplicates are handled by - let's say there is s and e index. 
    Now given nums[s] there is a unique nums[e] such that nums[s]+nums[e]=Target. Therefore, if nums[s+1] is the same as nums[s], then searching in range s+1 to e will give us a duplicate solution. 
    Thus we must move s till nums[s] != nums[s-1] to avoid getting duplicates.
                            while s<e and nums[s] == nums[s-1]:
                                s = s+1
    
    Handling Duplicates in 3SUM
    
    Imagine we are at index i and we have invoked the 2SUM problem from index i+1 to end of the array. Now once the 2SUM terminates, we will have a list of all triplets which include nums[i]. To avoid duplicates, we must skip all nums[i] where nums[i] == nums[i-1].
                if i > 0 and nums[i] == nums[i-1]:
                    continue

--- Arrays/3SUM.py
+++ Arrays/3SUM.py
@@ -1,20 +1,4 @@
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero. 
-Notice that the solution set must not contain duplicate triplets.
 
-Solution:
-
-In order to solve this question, I first reduced it to 2SUM problem by doing a+b = -c and the duplicates are handled by - let's say there is s and e index. 
-Now given nums[s] there is a unique nums[e] such that nums[s]+nums[e]=Target. Therefore, if nums[s+1] is the same as nums[s], then searching in range s+1 to e will give us a duplicate solution. 
-Thus we must move s till nums[s] != nums[s-1] to avoid getting duplicates.
-                        while s<e and nums[s] == nums[s-1]:
-                            s = s+1
-
-Handling Duplicates in 3SUM
-
-Imagine we are at index i and we have invoked the 2SUM problem from index i+1 to end of the array. Now once the 2SUM terminates, we will have a list of all triplets which include nums[i]. To avoid duplicates, we must skip all nums[i] where nums[i] == nums[i-1].
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-             
 class Solution:
 def threeSum (self, nums:List [int]) -> List [List[int]]:
   nums.sort()
